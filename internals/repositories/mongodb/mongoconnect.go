62947fface93f15da397a96e6c4e23b03ef1bbf5
aayushxrj <aayush.ranjan0904@gmail.com>
2025-09-20 18:52:30 +0530

    add error handler

--- internals/repositories/mongodb/mongoconnect.go
+++ internals/repositories/mongodb/mongoconnect.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aayushxrj/go-gRPC-api-school-mgmt/pkg/utils"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -12,14 +13,12 @@ func CreateMongoClient(ctx context.Context) (*mongo.Client, error) {
 	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("username:password@mongodb://localhost:27017"))
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		fmt.Println("Error connecting to MongoDB:", err)
-		return nil, err
+		return nil, utils.ErrorHandler(err, "Error connecting to MongoDB")
 	}
 
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		fmt.Println("Error pinging MongoDB:", err)
-		return nil, err
+		return nil, utils.ErrorHandler(err, "Error pinging MongoDB")
 	}
 
 	fmt.Println("Connected to MongoDB!")
