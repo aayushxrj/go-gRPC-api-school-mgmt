e1704e9219e41a2298a9f89522b89ce2769ebac7
aayushxrj <aayush.ranjan0904@gmail.com>
2025-10-19 17:43:03 +0530

    add logout handler

--- internals/api/handlers/execs.go
+++ internals/api/handlers/execs.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/aayushxrj/go-gRPC-api-school-mgmt/internals/models"
 	"github.com/aayushxrj/go-gRPC-api-school-mgmt/internals/repositories/mongodb"
 	"github.com/aayushxrj/go-gRPC-api-school-mgmt/pkg/utils"
 	pb "github.com/aayushxrj/go-gRPC-api-school-mgmt/proto/gen"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 )
 
@@ -197,5 +202,40 @@ func (s *Server) ResetPassword(ctx context.Context, req *pb.ResetPasswordRequest
 
 	return &pb.Confirmation{
 		Confirmation: true,
+	}, nil
+}
+
+func (s *Server) Logout(ctx context.Context, req *pb.EmptyRequest) (*pb.ExecLogoutResponse, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "no metadata found")
+	}
+
+	val, ok := md["authorization"]
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "Unauthorized Access")
+	}
+
+	token := strings.TrimPrefix(val[0], "Bearer ")
+
+	if token == "" {
+		return nil, status.Error(codes.Unauthenticated, "Unauthorized Access")
+	}
+
+	expiryTimeStamp := ctx.Value(utils.ContextKey("expiresAt"))
+	expiryTimeStr := fmt.Sprintf("%v", expiryTimeStamp)
+
+	expiryTimeInt, err := strconv.ParseInt(expiryTimeStr, 10, 64)
+	if err != nil {
+		utils.ErrorHandler(err, "")
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	expirytime := time.Unix(expiryTimeInt, 0)
+
+	utils.JwtStore.AddToken(token, expirytime)
+
+	return &pb.ExecLogoutResponse{
+		LoggedOut: true,
 	}, nil
 }
